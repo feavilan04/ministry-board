6620d763144dfbc3e05e416d69e52b18caf10a29
feavilan04 <feavilan@gmail.com>
2021-04-16 17:04:09 -0500

    fixed name of folder to store databases

--- main/db/manager/nedb.js
+++ main/db/manager/nedb.js
@@ -4,11 +4,11 @@ const path = require('path')
 var datastores={}
 
 datastores.publisher= new nedb({ 
-    filename: path.join('../../../', 'data', 'publishers.db'), 
+    filename: path.join(__dirname, '..','..','..','data','publishers.db'), 
     autoload: true, 
     timestampData: true })
 datastores.cleaning = new nedb({ 
-    filename: path.join('../../../', 'data', 'cleaning.db'), 
+    filename: path.join(__dirname, '..','..','..','data','cleaning.db'), 
     autoload: true, 
     timestampData: true})
 
